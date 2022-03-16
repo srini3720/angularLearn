d0a87acdcedc821c7f1d7dc0c316a435646df454
srinivasan <srini3720@gmail.com>
2022-03-16 15:31:13 +0530

    UI setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { MovieCardComponent } from './ui/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MovieCardComponent,
     
   ],
   imports: [
